9f294b7b73ff9d09a23fca3edf100a95e23b3ede
esoyeon <nplsoyeon@gmail.com>
2020-08-09 23:24:10 +0900

    py로 바꾸고 functions에 함수 추가

--- functions.py
+++ functions.py
@@ -5,6 +5,17 @@ from gensim import models
 import matplotlib.pyplot as plt
 import os
 from tensorflow import keras
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import pickle
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from IPython.display import SVG
+from tensorflow.python.keras.utils.vis_utils import model_to_dot
 
 
 def preprocessing(data):
@@ -125,5 +136,113 @@ def model_1():
     # model = keras.Sequential([keras.layers.Dense(1)])
     # model.compile(optimizer='adam', loss='mse')
 
-
     return model 
+
+
+######################################
+###모델3_contextualized_embedding#####
+######################################
+def load_data(path):
+    test = pd.read_pickle(path +'/'+ "token_test_data.pkl")
+    train = pd.read_pickle(path +'/' + "token_train_data.pkl")
+    
+    training_sentences = train['tokens']
+    testing_sentences = test['tokens']
+
+    training_labels = train['labels']
+    testing_labels = test['labels']
+
+    return training_sentences, testing_sentences, training_labels, testing_labels
+
+def tokenize_and_pad_model3(path, vocab_size = 20000, embedding_dim = 100, max_length = 30, padding_type = 'post'):
+  truct_type = 'post'
+  oov_tok = '<OOV>'
+  training_sentences, testing_sentences, training_labels, testing_labels = load_data(path)
+  tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+  tokenizer.fit_on_texts(training_sentences)
+  word_index = tokenizer.word_index
+
+  training_sequences  = tokenizer.texts_to_sequences(training_sentences)
+  training_padded = pad_sequences(training_sequences, maxlen=max_length, 
+                                  padding=padding_type, truncating=truct_type)
+
+  testing_sequences  = tokenizer.texts_to_sequences(testing_sentences)
+  testing_padded = pad_sequences(testing_sequences, maxlen=max_length, 
+                                  padding=padding_type, truncating=truct_type)
+  training_padded = np.array(training_padded)
+  training_labels = np.array(training_labels)
+
+  testing_padded = np.array(testing_padded)
+  testing_labels = np.array(testing_labels)
+
+  return training_padded, training_labels, testing_padded, testing_labels
+
+def model3_context(path, dropout = 0.5, embedding_dim = 100, max_length=30, batch_size = 50, num_epochs = 10, l2_norm=0.003):
+  filter_sizes = (3, 4, 5)
+  num_filters = 100
+  hidden_dims = 100
+  vocab_size = 20000
+
+  training_padded, training_labels, testing_padded, testing_labels = tokenize_and_pad_model3(path)
+
+  conv_blocks =[]
+  input_shape = (max_length, )
+  model_input = tf.keras.layers.Input(shape=input_shape)
+  z = model_input
+  for sz in filter_sizes:
+      embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length)(z)
+      conv = tf.keras.layers.Conv1D(filters=num_filters,
+                          kernel_size=sz,
+                          padding="valid",
+                          activation="relu",
+                          strides=1)(embedding)
+      conv = tf.keras.layers.GlobalAveragePooling1D()(conv)
+      conv = tf.keras.layers.Flatten()(conv)
+      conv_blocks.append(conv)
+  z = tf.keras.layers.Concatenate()(conv_blocks) if len(conv_blocks) > 1 else conv_blocks[0]
+
+  z = tf.keras.layers.Dense(hidden_dims, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(l2_norm), bias_regularizer=tf.keras.regularizers.l2(l2_norm))(z)
+  z = tf.keras.layers.Dropout(dropout)(z)
+  model_output = tf.keras.layers.Dense(1, activation="sigmoid")(z)
+
+  model = tf.keras.Model(model_input, model_output)
+
+  print(model.summary())
+
+  model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+  early_stopping=tf.keras.callbacks.EarlyStopping(patience=2)
+  history = model.fit(training_padded, training_labels, epochs=num_epochs, validation_data=(testing_padded, testing_labels), callbacks=[early_stopping], batch_size = batch_size)
+
+  return model, history
+
+def plot_accuracy_graphs(history, string='accuracy'):
+  plt.plot(history.history[string])
+  plt.plot(history.history['val_' + string])
+  plt.xlabel("Epochs")
+  plt.ylabel(string)
+  plt.legend([string, 'val_' + string])
+  plt.show()
+
+def plot_loss_graphs(history, string='loss'):
+  plt.plot(history.history[string])
+  plt.plot(history.history['val_' + string])
+  plt.xlabel("Epochs")
+  plt.ylabel(string)
+  plt.legend([string, 'val_' + string])
+  plt.show()
+
+
+
+# model 3 실행법 
+path = os.getcwd() #이런식으로 데이터 파일이 있는 path 지정 
+
+model, history = model3_context(path) #모델실행 
+
+#모델 그래프, 정확도 그래프, loss그래프 출력 
+SVG(model_to_dot(model, show_layer_names=True, dpi=65).create(prog='dot', format='svg'))
+plot_accuracy_graphs(history)
+plot_loss_graphs(history)
+    
+
+
+
